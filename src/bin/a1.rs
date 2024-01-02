0efb1a6f655e822b9469cdea47c70375e0570815
Johnsonojo <johnsonojo@gmail.com>
2024-01-02 16:52:57 +0100

    added functions task

---
+++ src/bin/a1.rs
@@ -0,0 +1,27 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+fn display_first_name() {
+    println!("My first name is Johnson");
+}
+
+fn display_last_name() {
+    println!("My last name is Ojo");
+}
+
+fn display_full_name() {
+    println!("My full name is Johnson Ojo");
+}
+
+fn main() {
+    display_first_name();
+    display_last_name();
+    display_full_name();
+}
