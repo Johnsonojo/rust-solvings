a3f4d1cde596a9ebf72864fc266b2afc364e697d
Johnsonojo <johnsonojo@gmail.com>
2024-01-09 18:09:41 +0100

    added loop activity

---
+++ src/bin/a5.rs
@@ -0,0 +1,26 @@
+// Topic: Looping using the loop statement
+//
+// Program requirements:
+// * Display "1" through "4" in the terminal
+//
+// Notes:
+// * Use a mutable integer variable
+// * Use a loop statement
+// * Print the variable within the loop statement
+// * Use break to exit the loop
+
+fn main() {
+    // * Use a mutable integer variable
+    let mut i: i32 = 1;
+    
+    // * Use a loop statement
+    loop {
+        // * Print the variable within the loop statement
+        println!("{:?}", i);
+        if i == 4 {
+            // * Use break to exit the loop
+            break;
+        }
+        i += 1; 
+    }
+}
